9fad3285f060c30a3e47e8996e007eabbefff214
Alexandre Ramos <alee.ram@hotmail.com>
2020-09-12 01:37:20 -0300

    commit inicial

---
+++ src/test/java/br/gov/sp/fatec/projetovagas/AppTest.java
@@ -0,0 +1,17 @@
+package br.gov.sp.fatec.projetovagas;
+
+import org.junit.Test;
+import static org.junit.Assert.*;		
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    
+    @Test 
+    public void testSoma()
+    {
+    	assertTrue(true);
+    }
+}
