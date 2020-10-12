1269aa75de06c70656d45bd3553e96db35af10b8
Alexandre Ramos <43897137+alexandreeufrazio@users.noreply.github.com>
2020-10-12 09:49:12 -0300

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -2,48 +2,40 @@
 
 -- drop schema vagas;
 
-create schema vagas;
+create schema vaga;
 
-use vagas;
+use vaga;
 
 create user 'user'@'localhost' identified by 'pass123';
 
-grant select, insert, delete, update on vagas.* to user@'localhost';
+grant select, insert, delete, update on vaga.* to user@'localhost';
 
+create table emp_empresa (
+	emp_id bigint unsigned primary key auto_increment,
+	emp_razao_social varchar(50) not null,
+	emp_senha varchar(50) not null,
+	constraint emp_razao_social_uk unique (emp_razao_social)
+);
 create table usu_usuario (
 	usu_id bigint unsigned primary key auto_increment,
 	usu_nome_usuario varchar(50) not null,
-	usu_senha varchar(50) not null,
-	constraint usu_nome_usuario_uk unique (usu_nome_usuario)
+	alu_senha varchar(50) not null,
+	constraint usu_nome_usuario_uk unique (usu_nome_usuario),
+	
 );
-
-create table emp_empresa (
-	emp_id bigint unsigned primary key,
-	emp_cnpj varchar(14) not null,
-	emp_nome_fantasia varchar(50) not null,
-	emp_responsavel varchar(100) not null,
-	emp_telefone varchar(15) not null,
-	constraint emp_usu_fk foreign key(emp_id)
-		references usu_usuario(usu_id)
-);
-
-create table vag_vagas (
-	vag_id bigint unsigned primary key,
-	vag_cargo varchar(50) not null,
-	vag_descricao varchar(500) not null,
-	vag_status varchar(10) not null,
-	constraint vag_emp_fk foreign key (vag_id)
+create table vag_vaga (
+	vag_id bigint unsigned primary key auto_increment,
+	vag_descricao varchar(200) not null,
+	emp_anunciante_id bigint unsigned,
+	constraint vag_emp_fk foreign key (emp_anunciante_id)
 	references emp_empresa (emp_id)
 );
-
-create table end_endereco (
-	end_id bigint unsigned primary key,
-	end_tipo varchar(10) not null,
-	end_tipo_descricao varchar(50),
-	end_numero varchar(10),
-	end_cidade varchar(20),
-	end_estado varchar(20),
-	end_pais varchar(20),
-	constraint end_emp_fk foreign key (end_id)
+create table anu_anuncio (
+	usu_id bigint unsigned,
+	emp_id bigint unsigned,
+	primary key (usu_id, emp_id),
+	constraint anu_usu_fk foreign key (usu_id)
+	references usu_usuario (usu_id),
+	constraint vag_emp_fk foreign key (emp_id)
 	references emp_empresa (emp_id)
 );
