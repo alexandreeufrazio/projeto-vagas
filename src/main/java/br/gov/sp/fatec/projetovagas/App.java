c2fd4482ec581b01d432d2daba947d9f5237c16a
alexandreeufrazio <alee.ram@hotmail.com>
2020-10-13 02:37:25 +0000

    java

--- src/main/java/br/gov/sp/fatec/projetovagas/App.java
+++ src/main/java/br/gov/sp/fatec/projetovagas/App.java
@@ -1,8 +1,13 @@
 package br.gov.sp.fatec.projetovagas;
 
+import java.util.HashSet;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
+
+import br.gov.sp.fatec.projetovagas.entity.Empresa;
 import br.gov.sp.fatec.projetovagas.entity.Usuario;
 import br.gov.sp.fatec.projetovagas.entity.Vaga;
 
@@ -21,13 +26,13 @@ public class App {
         Empresa empresa = new Empresa();
         empresa.setRazao_social("IBM");
         empresa.setSenha("senhaF0rte");
-        /*
+        
         Vaga vaga = new Vaga();
         vaga.setDescricao("Analista de Sistemas");
         vaga.setAnunciante(empresa);
         vaga.setUsuarios(new HashSet<Usuario>());
         vaga.getUsuarios().add(usuario);
-        */
+        
         try{
             manager.getTransaction().begin();
             manager.persist(usuario);
@@ -39,8 +44,8 @@ public class App {
             manager.getTransaction().rollback();
         }
 
-        //manager.clear();
-/*
+        manager.clear();
+
         usuario = manager.find(Usuario.class, usuario.getId);
         System.out.println(usuario.getId());
         System.out.println(usuario.getNomeUsuario());
@@ -48,13 +53,13 @@ public class App {
 	        System.out.println(vaga1.getDescricao());
         }
 
-       // manager.clear();
+        manager.clear();
         
         vaga = manager.find(Vaga.class, vaga.getId);
         System.out.println(vaga.getDescricao());
         for(Usuario usuario1: vaga.getUsuarios()){
 	        System.out.println(usuario1.getNomeUsuario());
-        }*/
+        }
 
         manager.close();
 
