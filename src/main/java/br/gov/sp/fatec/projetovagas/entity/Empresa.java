9a8c4584118e4b50ad50e7bfe779d31164a22a0b
alexandreeufrazio <alee.ram@hotmail.com>
2020-10-29 23:36:05 +0000

    JPA

--- src/main/java/br/gov/sp/fatec/projetovagas/entity/Empresa.java
+++ src/main/java/br/gov/sp/fatec/projetovagas/entity/Empresa.java
@@ -5,37 +5,18 @@ import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Table(name = "emp_empresa")
 @Entity
-public class Empresa{
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "emp_id")
-    private Long id;
+public class Empresa extends Conta{
 
     @Column(name = "emp_razao_social")
     private String razao_social;
 
-    @Column(name = "emp_senha")
-    private String senha;
-
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "anunciante")
     private Set<Vaga> vagasAnunciadas;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getRazao_social() {
         return razao_social;
@@ -43,14 +24,6 @@ public class Empresa{
 
     public void setRazao_social(String razao_social) {
         this.razao_social = razao_social;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
     }
 
     public Set<Vaga> getVagasAnunciadas() {
--- src/main/java/br/gov/sp/fatec/projetovagas/entity/Usuario.java
+++ src/main/java/br/gov/sp/fatec/projetovagas/entity/Usuario.java
@@ -1,57 +1,17 @@
 package br.gov.sp.fatec.projetovagas.entity;
 
 import java.util.Set;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Table(name = "usu_usuario")
 @Entity
-public class Usuario {
+public class Usuario extends Conta {
    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "usu_id")
-    private Long id;
-  
-    @Column(name = "usu_nome_usuario")
-    private String nomeUsuario;
-
-    @Column(name = "usu_senha")
-    private String senha;
-
     @ManyToMany(fetch = FetchType.LAZY, mappedBy = "usuarios")
     private Set<Vaga> vagas;
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	public String getNomeUsuario() {
-		return nomeUsuario;
-	}
-
-	public void setNomeUsuario(String nomeUsuario) {
-		this.nomeUsuario = nomeUsuario;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-    }
 
     public Set<Vaga> getVagas() {
         return vagas;
